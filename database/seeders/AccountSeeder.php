c50d94bb0ece49d42faa7e0981957386007b7714
Sebastian <denzilbao@gmail.com>
2021-04-02 13:47:18 +0800

    save progress.

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Account;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AccountSeeder extends Seeder
