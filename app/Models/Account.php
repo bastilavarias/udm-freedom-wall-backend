be682b852889cbf073d6ae06e05af567ae9ceadc
Sebastian <denzilbao@gmail.com>
2021-04-02 13:55:26 +0800

    save progress.

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Authenticatable
 {
-    use HasFactory, HasApiTokens, Notifiable;
+    use HasFactory, HasApiTokens, Notifiable, SoftDeletes;
 
     protected $table = "accounts";
 
