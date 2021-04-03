c94bee320e5fabed158ce76193da6d42861f23c8
Sebastian <denzilbao@gmail.com>
2021-04-03 12:46:25 +0800

    save progress.

--- app/Http/Controllers/api/v2/PeopleTypeController.php
+++ app/Http/Controllers/api/v2/PeopleTypeController.php
@@ -6,14 +6,21 @@ use App\Common\Helper;
 use App\Http\Controllers\Controller;
 use App\Models\PeopleType;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class PeopleTypeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $page = $request->query("page");
+        $perPage = $request->query("per_page");
+        $search = $request->query("search");
+        $admins = PeopleType::where(
+            "label",
+            "ilike",
+            "%" . $search . "%"
+        )->paginate($perPage, ["*"], "page", $page);
+        return Helper::apiResponse(true, "Successfully got records.", $admins);
     }
 
     public function create(Request $request)
